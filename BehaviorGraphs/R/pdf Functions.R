9edf824380cc4697b4d2bb958c793144cb25f037
Jeff Brown <124228321+JeffreyDeanBrown@users.noreply.github.com>
2024-11-15 12:11:35 -0600

    Update pdf Functions.R

--- BehaviorGraphs/R/pdf Functions.R
+++ BehaviorGraphs/R/pdf Functions.R
@@ -20,12 +20,6 @@
 # source files
 
 #----------------------------------------------------------------------------------
-
-#setup the filename for the genotype of the day
-name_of_the_day <- capture.output(cat("PDFs/",genotype_of_the_day,".pdf",sep=""))
-cat(name_of_the_day)
-
-#----------------------------------------------------------------------------------
 print_with_indiv <- function() {
   print("Printing graphs...")
   dir.create("PDFs")
@@ -64,7 +58,7 @@ indiv_$s_rotarod_plot[which(str_detect(indiv_$group,genotype_of_the_day))]      
 indiv_$s_rotarod_plot[which(str_detect(indiv_$group,genotype_of_the_day))]                 ,
 indiv_$condition_plot[which(str_detect(indiv_$group,genotype_of_the_day))]) 
 
-ggsave(filename = name_of_the_day, plot = marrangeGrob(grobs = list, nrow = 1, ncol = 1),
+ggsave(filename = filename_of_the_day, plot = marrangeGrob(grobs = list, nrow = 1, ncol = 1),
        width = 7, height = 7)
 
 
@@ -128,7 +122,7 @@ print_without_indiv <- function(){
   genotype_group$t_rotarod_plot[which(str_detect(genotype_group$group,genotype_of_the_day))] ,
   genotype_group$condition_plot[which(str_detect(genotype_group$group,genotype_of_the_day))]) 
   
-  ggsave(filename = name_of_the_day, plot = marrangeGrob(grobs = list, nrow = 1, ncol = 1),
+  ggsave(filename = filename_of_the_day, plot = marrangeGrob(grobs = list, nrow = 1, ncol = 1),
          width = 7, height = 7)
   
   
